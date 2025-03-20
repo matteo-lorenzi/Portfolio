347df0d46593852be7429bf1223a763c350ef386
Matteo Lorenzi <matteo.lorenzi@etu.univ-grenoble-alpes.fr>
2025-03-20 13:31:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AllProjects from './pages/AllProjects';
+import ProjectPage from './pages/ProjectPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/all-projects" element={<AllProjects />} />
+        <Route path="/projects/:id" element={<ProjectPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
